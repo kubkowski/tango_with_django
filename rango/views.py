8eaade48a9cd1dbb7411f1d58d68c5bf6e8f75d1
kubkowski <jakub.opara@gmail.com>
2015-02-10 10:10:24 +0100

    Added user and userprofile forms + view

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm, PageForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 # Create your views here.
 def index(request):
@@ -78,6 +76,38 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form, 'category': cat, 'slug': category_name_slug}
 
     return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid:
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+        'rango/register.html',
+        { 'user_form': user_form, 'profile_form': profile_form, 'registered': registered }
+    )
 
 
 
