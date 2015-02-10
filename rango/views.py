dd129c7ff38b9fde714d1aa26817a993966fdd4b
kubkowski <jakub.opara@gmail.com>
2015-02-10 18:03:19 +0100

    Added login view, template and route

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
@@ -108,6 +109,26 @@ def register(request):
         'rango/register.html',
         { 'user_form': user_form, 'profile_form': profile_form, 'registered': registered }
     )
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'rango/login.html', {})
 
 
 
